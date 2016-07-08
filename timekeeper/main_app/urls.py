8678ae4995bc717e21c0c51b48d4caac3b744eff
ivenn <ivenn@mail.ru>
2016-07-08 16:50:42 +0300

    Registration sketch

--- timekeeper/main_app/urls.py
+++ timekeeper/main_app/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^main/$', views.user_main, name='main'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^personal/$', views.user_personal, name='personal'),
     url(r'^settings/$', views.user_settings, name='settings'),
     url(r'^reports/$', views.user_reports, name='reports'),
+    url(r'^registration/$', views.user_registration, name='registration'),
+
+    url(r'^$', views.index, name='index'),
 ]
--- timekeeper/main_app/views.py
+++ timekeeper/main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .models import Settings
+
+
+def index(request):
+    return render(request, 'index.html', {})
 
 
 def user_login(request):
@@ -16,8 +21,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect('main')
-                    return HttpResponse('Authenticated successfully')
+                    return redirect('personal')
                 else:
                     return HttpResponse('Disabled account')
             else:
@@ -28,10 +32,25 @@ def user_login(request):
     return render(request, 'login.html', {'form': form})
 
 
+def user_registration(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Settings.objects.create(owner=new_user)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration.html', {'form': form})
+
+
 @login_required
-def user_main(request):
+def user_personal(request):
     username = request.user.username
-    return render(request, 'main.html', {'username': username})
+    return render(request, 'personal.html', {'username': username})
 
 
 @login_required
@@ -44,3 +63,10 @@ def user_settings(request):
 def user_reports(request):
     username = request.user.username
     return render(request, 'reports.html', {'username': username})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('index')
+
