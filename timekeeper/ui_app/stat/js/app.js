2e938267931799b3cc34d1a5c5facffa3153e3b4
Va11ter <va77ter@gmail.com>
2016-07-26 00:43:35 +0300

    One more step to first ru

--- timekeeper/ui_app/stat/js/app.js
+++ timekeeper/ui_app/stat/js/app.js
@@ -1,6 +1,11 @@
 var app = (function ($) {
-    var  config = $('config'),
+    var  config = $('#config'),
         app = JSON.parse(config.text());
 
+    $(document).ready(function () {
+        var router = new app.router();
+    });
+    console.log("Application is started");
     return app;
-})(jQuery)
+
+})(jQuery);
