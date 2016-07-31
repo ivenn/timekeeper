7f4e11b182407916b4419953fd98caf0917a5b23
Va11ter <va77ter@gmail.com>
2016-08-01 00:23:42 +0300

    Fix misprint/add missed code to models.js

--- timekeeper/ui_app/stat/js/models.js
+++ timekeeper/ui_app/stat/js/models.js
@@ -1,42 +1,43 @@
 (function ($, Backbone, _, app) {
-	// CSRF helper functions taken directly from Django docs
-	function csrfSafeMethod(method){
-		// these HTTP methods do no require CSRF protection
-		return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method))
-	}
+    // CSRF helper functions taken directly from Django docs
+    function csrfSafeMethod(method){
+        // these HTTP methods do no require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method))
+    }
 
-	function getCookie(name){
-		var cookieValue = null;
-		if (document.cookie && document.cookie != ''){
-			var cookies = document.cookie.split(';');
-			for (var i = 0; i <cookies.lenght; i++){
-				var cookie = $.trim(cookies[i]);
-				// Does this cookie string begin with the name we want?
-				if (cookie.substring(0, name.lenght + 1) == (name + '=')) {
-					cookieValue = decodeURIComponent(
-						cookie.substring(name.lenght + 1));
+    function getCookie(name){
+        var cookieValue = null;
+        if (document.cookie && document.cookie != ''){
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++){
+                var cookie = $.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(
+                        cookie.substring(name.length + 1));
 
-					break;
-				}
-			}
-		}
-		return cookieValue;
-	}
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
 
-	// Setup jQuerry ajax calls to handle CSRF
-	$.ajaxPrefilter(function (settings, originalOptions, xhr)){
-		var csrftoken;
-		if (!csrfSafeMethod(settings.type) && !this.crossDomain){
-			// Send the token to same-origin, relative URLs only.
-			// Send the token only if the method warrants CSRF protection
-			// Using the CSRFToken value acquired earlier
-			
-		}
+    // Setup jQuerry ajax calls to handle CSRF
+    $.ajaxPrefilter(function (settings, originalOptions, xhr){
+        var csrftoken;
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain){
+            // Send the token to same-origin, relative URLs only.
+            // Send the token only if the method warrants CSRF protection
+            // Using the CSRFToken value acquired earlier
+            csrftoken = getCookie('csrftoken');
+            xhr.setRequestHeader('X-CSRFToken', csrftoken)
+        }
 
-	}
+    });
 
 
-    var Sesion = Beckbone.Model.extend({
+    var Sesion = Backbone.Model.extend({
         default: {
             token: null
         },
@@ -66,12 +67,12 @@
             return this.get('toke') !== null;
         },
         _setupAuth: function (settings, originalOptions, xhr) {
-        	if (this.authenticated()){
-        		xhr.setRequestHeader(
-        			'Authorization',
-        			'Token ' + this.get('token')
-        		);
-        	}
+            if (this.authenticated()){
+                xhr.setRequestHeader(
+                    'Authorization',
+                    'Token ' + this.get('token')
+                );
+            }
         }
 
     });
