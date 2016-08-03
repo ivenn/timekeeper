21cb701e351729c7a74e20dd7de04b26a1e8d3e5
Va11ter <va77ter@gmail.com>
2016-08-03 15:25:07 +0300

    Move ui app to timekeeper folder

--- timekeeper/timekeeper/urls.py
+++ timekeeper/timekeeper/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -9,4 +10,5 @@ from main_app.urls import router
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='ui_app/index.html'))
 ]
